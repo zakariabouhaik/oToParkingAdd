95eb52306c5d4e98e9332a2934aeeb1860800a29
zakariabouhaik <162756859+zakariabouhaik@users.noreply.github.com>
2024-10-16 19:29:55 +0100

    Update ProtectedRoute.jsx

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -10,11 +10,17 @@ const ProtectedRoute = ({ children, requiredRoles }) => {
         return <div>Loading...</div>;
     }
 
-    if (!userDetails || !requiredRoles.includes(userDetails.role)) {
-   
+    if (!userDetails) {
+        // User is not authenticated, redirect to login page
         return <Navigate to="/" state={{ from: location }} replace />;
     }
 
+    if (!requiredRoles.includes(userDetails.role)) {
+        // User doesn't have the required role, redirect to login page
+        return <Navigate to="/" state={{ from: location }} replace />;
+    }
+
+    // User is authenticated and has the required role, render the protected content
     return children;
 };
 
